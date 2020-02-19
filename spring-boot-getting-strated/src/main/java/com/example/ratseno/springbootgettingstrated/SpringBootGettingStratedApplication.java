4891b4ecba3d53a360bf9a4c0316ad2e9f7d72d7
RATSENO <onestar1014@gmail.com>
2020-02-19 10:43:05 +0900

    srping boot 프로젝트 생성

---
+++ spring-boot-getting-strated/src/main/java/com/example/ratseno/springbootgettingstrated/SpringBootGettingStratedApplication.java
@@ -0,0 +1,13 @@
+package com.example.ratseno.springbootgettingstrated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGettingStratedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGettingStratedApplication.class, args);
+	}
+
+}
---
+++ spring-boot-getting-strated/src/test/java/com/example/ratseno/springbootgettingstrated/SpringBootGettingStratedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ratseno.springbootgettingstrated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGettingStratedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
