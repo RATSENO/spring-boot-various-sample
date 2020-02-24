373de4622c753411212ada377691f811357e8783
RATSENO <onestar1014@gmail.com>
2020-02-24 13:58:53 +0900

    자동 설정 만들기-2 spring-boot-starter-auto-config 프로젝트의 설정 부분에  @ConditionalOnMissingBean @Component 스캔 후 해당 빈이 등록이 안되있을때 등록(@EnableAutoConfiguration)

--- spring-boot-starter-auto-config/src/main/java/com/example/ratseno/HolomanCoufiguration.java
+++ spring-boot-starter-auto-config/src/main/java/com/example/ratseno/HolomanCoufiguration.java
@@ -1,12 +1,13 @@
 package com.example.ratseno;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HolomanCoufiguration {
-
     @Bean
+    @ConditionalOnMissingBean
     public Holoman holoman() {
         Holoman holoman = new Holoman();
         holoman.setHowLong(5);
