eaf5b60d29ac0896755956caf0b2f137fe021b8e
RATSENO <onestar1014@gmail.com>
2020-02-24 15:18:23 +0900

    내장 웹서버 응용 - jetty

--- spring-boot-web-server/src/main/java/org/example/ratseno/Application.java
+++ spring-boot-web-server/src/main/java/org/example/ratseno/Application.java
@@ -1,8 +1,8 @@
 package org.example.ratseno;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,33 +12,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 
+@SpringBootApplication
 public class Application {
-    public static void main(String[] args) throws LifecycleException, IOException {
-        Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
-
-        String docBase = Files.createTempDirectory("tomcat-basedir").toString();
-
-        Context context = tomcat.addContext("/", docBase);
-
-        HttpServlet httpServlet = new HttpServlet() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                PrintWriter writer = resp.getWriter();
-                writer.println("<html><head><title>");
-                writer.println("Hey, Tomcat");
-                writer.println("</title></head>");
-                writer.println("<body><h1>Hello Tomcat</h1></body>");
-                writer.println("</html>");
-            }
-        };
-
-        String servletName = "helloServlet";
-        tomcat.addServlet("/", servletName, httpServlet);
-        context.addServletMappingDecoded("/hello", servletName);
-
-        tomcat.getConnector();
-        tomcat.start();
-        tomcat.getServer().await();
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
