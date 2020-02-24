c30ad860f8539f9bd5ee5682c827899f756c990a
RATSENO <onestar1014@gmail.com>
2020-02-24 13:28:50 +0900

    자동 설정 만들기-1 spring-boot-starter-atuto-config에서 만든 설정을 spring-boot-getting-strated프로젝트에서 의존성을 주입받아서 등록하기 @EnableAutoConfiguration 단계에서 빈으로 등록된다.

--- spring-boot-getting-strated/src/main/java/com/example/ratseno/springbootgettingstrated/SpringBootGettingStratedApplication.java
+++ spring-boot-getting-strated/src/main/java/com/example/ratseno/springbootgettingstrated/SpringBootGettingStratedApplication.java
@@ -1,13 +1,15 @@
 package com.example.ratseno.springbootgettingstrated;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootGettingStratedApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootGettingStratedApplication.class, args);
-	}
-
+        SpringApplication springApplication = new SpringApplication(SpringBootGettingStratedApplication.class);
+        springApplication.setWebApplicationType(WebApplicationType.NONE);
+        springApplication.run(args);
+    }
 }
---
+++ spring-boot-starter-auto-config/src/main/java/com/example/ratseno/HolomanCoufiguration.java
@@ -0,0 +1,16 @@
+package com.example.ratseno;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HolomanCoufiguration {
+
+    @Bean
+    public Holoman holoman() {
+        Holoman holoman = new Holoman();
+        holoman.setHowLong(5);
+        holoman.setName("ratseno");
+        return holoman;
+    }
+}
