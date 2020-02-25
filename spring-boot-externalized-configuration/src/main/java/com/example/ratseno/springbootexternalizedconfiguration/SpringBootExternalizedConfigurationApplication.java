7c0b281cd2b2434a49e7c40940dadbe9f60026a8
RATSENO <onestar1014@gmail.com>
2020-02-25 14:07:08 +0900

    외부 설정

---
+++ spring-boot-externalized-configuration/src/main/java/com/example/ratseno/springbootexternalizedconfiguration/SpringBootExternalizedConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.ratseno.springbootexternalizedconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExternalizedConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExternalizedConfigurationApplication.class, args);
+	}
+
+}
---
+++ spring-boot-externalized-configuration/src/test/java/com/example/ratseno/springbootexternalizedconfiguration/SpringBootExternalizedConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ratseno.springbootexternalizedconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExternalizedConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
