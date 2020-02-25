899bce17e96e988b0004096efc249c28a9ed7ed7
RATSENO <onestar1014@gmail.com>
2020-02-25 17:04:56 +0900

    spring profile -2 application-{env}.properties 환경별로 만들어둔 프로퍼티의 우선순위가 application.properties내부의 셋팅값보다 높다. mvn clean mvn package java -jar target/spring-boot-profile-0.0.1-SNAPSHOT.jar -Dspring.profiles.active=prod

--- spring-boot-profile/src/main/java/com/example/ratseno/springbootprofile/SampleRunner.java
+++ spring-boot-profile/src/main/java/com/example/ratseno/springbootprofile/SampleRunner.java
@@ -3,6 +3,7 @@ package com.example.ratseno.springbootprofile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,10 +12,14 @@ public class SampleRunner implements ApplicationRunner {
     @Autowired
     private String hello;
 
+    @Autowired
+    private Environment environment;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("========================");
         System.out.println(hello);
+        System.out.println(environment.getProperty("ratseno.name"));
         System.out.println("========================");
     }
 }
