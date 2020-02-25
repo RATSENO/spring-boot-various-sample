d6af4a0854e3ffc71729418f05ff6d30bf06215a
RATSENO <onestar1014@gmail.com>
2020-02-25 11:00:51 +0900

    ApplicationEvent 등록 ApplicationContext를 만들기 전에 사용하는 Listener는 @Bean으로 등록할 수 없다. 메인 클래스트의 SpringApplication.addListener()를 사용하여 추가한다.

--- spring-boot-application/src/main/java/com/example/ratseno/springbootapplication/Application.java
+++ spring-boot-application/src/main/java/com/example/ratseno/springbootapplication/Application.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication springApplication = new SpringApplication(Application.class);
+		springApplication.addListeners(new SampleListener());
+		springApplication.run(args);
+
+		//SpringApplication.run(Application.class, args);
 	}
 
 }
