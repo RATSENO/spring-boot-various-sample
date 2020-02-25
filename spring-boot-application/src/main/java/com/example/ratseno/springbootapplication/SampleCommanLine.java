4e190834486756dce09956715579c07b7cb10cb6
RATSENO <onestar1014@gmail.com>
2020-02-25 11:33:05 +0900

    순서 지정 @Order(1) >  숫자가 낮을수록 우선순위 높다

--- spring-boot-application/src/main/java/com/example/ratseno/springbootapplication/SampleCommanLine.java
+++ spring-boot-application/src/main/java/com/example/ratseno/springbootapplication/SampleCommanLine.java
@@ -1,11 +1,13 @@
 package com.example.ratseno.springbootapplication;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Component
+@Order(1)
 public class SampleCommanLine implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
