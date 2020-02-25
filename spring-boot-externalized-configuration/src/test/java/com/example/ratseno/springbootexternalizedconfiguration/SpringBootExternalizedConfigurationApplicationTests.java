36a3cc1b99187bceeecd2837534b9361199d691d
RATSENO <onestar1014@gmail.com>
2020-02-25 15:40:17 +0900

    외부 설정 - test/resources/test.properties 파일 생성 test.properties의 우선순위가 높기때문에 application.properties의 name을 덮어쓴다.
    main/resources/application.properties 파일과
    test/resources/test.properties파일의 이름이 다르기 때문에
    오버라이드 되지않고 두 설정파일이 존재한다

--- spring-boot-externalized-configuration/src/test/java/com/example/ratseno/springbootexternalizedconfiguration/SpringBootExternalizedConfigurationApplicationTests.java
+++ spring-boot-externalized-configuration/src/test/java/com/example/ratseno/springbootexternalizedconfiguration/SpringBootExternalizedConfigurationApplicationTests.java
@@ -12,18 +12,29 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@TestPropertySource(properties = "ratseno.name=test2")
+@TestPropertySource(locations = "classpath:/test.properties")
 public class SpringBootExternalizedConfigurationApplicationTests {
 
 	@Autowired
 	Environment environment;
 
-	//test/resources/application.properties 파일이 있을경우
-	//main/resources/application.properties 파일을 엎어쓴다.
 	@Test
 	public void contextLoads() {
+
+		///test.properties의 우선순위가 높기때문에
+		//application.properties의 name을 덮어쓴다.
 		assertThat(environment.getProperty("ratseno.name"))
-				.isEqualTo("test2");
+				.isEqualTo("testName");
+	}
+
+	@Test
+	public void contextLoads2() {
+
+		//main/resources/application.properties 파일과
+		//test/resources/test.properties파일의 이름이 다르기 때문에
+		//오버라이드 되지않고 두 설정파일이 존재한다
+		assertThat(environment.getProperty("ratseno.height"))
+				.isEqualTo("191");
 	}
 
 }
