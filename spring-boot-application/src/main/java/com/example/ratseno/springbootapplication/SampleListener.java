d1c7f452cdb39dbfbcc830c7b14e55abb281ce10
RATSENO <onestar1014@gmail.com>
2020-02-25 11:04:35 +0900

    ApplicationListenr - ApplicationListener<ApplicationStartedEvent> ApplicationContext가 뜨고난 뒤이기 때문에 따로 이벤트를 등록하지 않고 @Component를 이용해 빈으로 등록하여 사용할 수 있다.

--- spring-boot-application/src/main/java/com/example/ratseno/springbootapplication/SampleListener.java
+++ spring-boot-application/src/main/java/com/example/ratseno/springbootapplication/SampleListener.java
@@ -1,15 +1,17 @@
 package com.example.ratseno.springbootapplication;
 
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.boot.context.event.ApplicationStartingEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
-public class SampleListener implements ApplicationListener<ApplicationStartingEvent> {
+@Component
+public class SampleListener implements ApplicationListener<ApplicationStartedEvent> {
 
     @Override
-    public void onApplicationEvent(ApplicationStartingEvent event) {
+    public void onApplicationEvent(ApplicationStartedEvent event) {
         System.out.println("==========================");
-        System.out.println("Application is starting");
+        System.out.println("Application is started");
         System.out.println("==========================");
     }
 }
