2af393481aa92d6863ee50e46e87d4823d27d7be
RATSENO <onestar1014@gmail.com>
2020-02-26 16:54:01 +0900

    test - 슬라이스테스트(레이어별로 테스트할때) @WebMvcTest(SampleController.class) SampleControllerTest만 빈으로 등록된다.

--- spring-boot-test/src/test/java/com/example/ratseno/springboottest/sample/SampleControllerTest.java
+++ spring-boot-test/src/test/java/com/example/ratseno/springboottest/sample/SampleControllerTest.java
@@ -3,29 +3,34 @@ package com.example.ratseno.springboottest.sample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(SampleController.class)
 public class SampleControllerTest {
-
-    @Autowired
-    WebTestClient webTestClient;
 
     @MockBean
     SampleService sampleService;
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
     public void hell() throws Exception {
         when(sampleService.getName()).thenReturn("onestar");
 
-        webTestClient.get().uri("/hello").exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class).isEqualTo("hello onestar");
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello onestar"))
+                .andDo(print());
     }
 }
