8a0c2e4337734ac2762f66f3edeacfacec7e94b5
RATSENO <onestar1014@gmail.com>
2020-02-26 10:40:26 +0900

    spring-boot-logger - logging.path.xxxx.xxxx 로그 레벨 조정 logging.level.패지키 = 로그 레벨

--- spring-boot-logger/src/main/java/com/example/ratseno/springbootlogger/SamplerRunner.java
+++ spring-boot-logger/src/main/java/com/example/ratseno/springbootlogger/SamplerRunner.java
@@ -1,15 +1,20 @@
 package com.example.ratseno.springbootlogger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SamplerRunner implements ApplicationRunner {
+
+    private Logger logger = LoggerFactory.getLogger(SamplerRunner.class);
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("==========run==========");
-
-        System.out.println("=======================");
+        logger.info("============================");
+        logger.info("hello");
+        logger.info("============================");
     }
 }
