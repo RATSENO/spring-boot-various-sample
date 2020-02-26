5ca66237aedd643f80479fca7a74cb7218216fa6
RATSENO <onestar1014@gmail.com>
2020-02-26 16:06:58 +0900

    add file

---
+++ spring-boot-test/src/test/java/com/example/ratseno/springboottest/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ratseno.springboottest;
+
+import org.junit.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
